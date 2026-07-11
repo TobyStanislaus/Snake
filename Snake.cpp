663b0423a4c80d0745b6463e22eeed09a614ecdb
Tobyisepik <toby@stanislaus.co.uk>
2026-07-11 16:09:16 +0100

    Working Snake Game

--- Snake.cpp
+++ Snake.cpp
@@ -5,7 +5,9 @@
 #include <chrono>
 #include <windows.h>
 #include <algorithm>
-
+#include <numeric>
+#include <random>
+#include <string>
 void clearScreen() {
     COORD topLeft = {0, 0};
     HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -27,10 +29,13 @@ void clearScreen() {
 const int width = 20;
 const int height = 10;
 
+std::random_device rd;
+std::mt19937 gen(rd());
 
 struct Point {
     int x;
     int y;
+    Point() = default;
     Point(int x_a, int y_a){
         x= x_a;
         y= y_a;
@@ -51,8 +56,18 @@ struct Snake{
     int y = 0;
     Body body;
     char direction = 'd';
+    std::vector<int> possi_width{width};
+    std::vector<int> possi_height{height};
 
-    Snake(){};
+
+    Snake(){
+        std::iota(possi_width.begin(), possi_width.end(), 0);
+        std::iota(possi_height.begin(), possi_height.end(), 0);
+
+        body.insert(body.begin(), Point{0,0});
+        body.insert(body.begin(), Point{0,0});
+        body.insert(body.begin(), Point{0,0});
+    };
       
     bool move(Field& field){
         if (direction == 'u'){y-=1;}
@@ -62,6 +77,17 @@ struct Snake{
         Point newpoint{x, y};
         if (!check_move(field, newpoint)){return false;}
         body.insert(body.begin(), newpoint);
+        
+        possi_width.erase(
+            std::remove(possi_width.begin(), possi_width.end(), x),
+            possi_width.end()
+        );
+
+        possi_height.erase(
+            std::remove(possi_height.begin(), possi_height.end(), y),
+            possi_height.end()
+        );
+
         return true;
     }
 
@@ -89,17 +115,43 @@ struct Snake{
         return field;
     }
 
+
+    bool check_fruit(Point fruit){
+        if ((x==fruit.x)&&(y==fruit.y)){return true;}
+        return false;
+    }
+
+
+    void spawn_fruit(Field& field, Point& fruit){
+        std::uniform_int_distribution<> w_dist(0, possi_width.size() - 1);
+        fruit.x = possi_width[w_dist(gen)];
+
+        std::uniform_int_distribution<> h_dist(0, possi_height.size() - 1);
+        fruit.y = possi_height[h_dist(gen)];
+    }
 };
+
+
+std::string create_line(int amount){
+    std::string line = "";
+    for (int i=0;i<amount;i++){
+        line=line+"──";
+    }
+    return line;
+}
 
 
 void display_field(Field field){
     clearScreen();
+    std::cout<< create_line(field[0].size()+1) <<std::endl;
     for (const auto&row : field){
+        std::cout << "|";
         for (const auto&square : row){
             std::cout<< square << ' ';
         }
-        std::cout<<"\n";
+        std::cout<<"|\n";
     }
+    std::cout<< create_line(field[0].size()+1) <<std::endl;
 }
 
 
@@ -109,7 +161,10 @@ int main(){
     bool survived = true;
     Field newfield;
     Field field(height, std::vector<char>(width, ' '));
+    Point remove_part{};
+    Point fruit{};
 
+    snake.spawn_fruit(field, fruit);
 
     while (true)
     {
@@ -123,10 +178,24 @@ int main(){
             else if (key == 'd'){snake.direction = 'r';}
         }
 
+        if (snake.check_fruit(fruit)){
+            snake.spawn_fruit(field, fruit);
+        }else{          
+        remove_part = snake.body.back();
+        snake.body.pop_back();
+
+        snake.possi_width.push_back(remove_part.x);
+        snake.possi_height.push_back(remove_part.y);
+        }
+
         survived = snake.move(field);
         newfield = snake.place_snake(field);
 
+        if (!(newfield[fruit.y][fruit.x] == '0')){
+            newfield[fruit.y][fruit.x] = 'A';
+        }
         display_field(newfield);
+
         if (!survived){
             std::cout<<"You Lost!";
             break;
